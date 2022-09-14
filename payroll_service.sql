eced180f52e9262849e971f9ddd6a07b80c8a7e2
mehulbhange <bhangemehul@gmail.com>
2022-09-14 14:38:20 +0530

    ADDED:[Mehul] added query to create database

---
+++ payroll_service.sql
@@ -0,0 +1,23 @@
+/*UC1*/
+mysql> create database payroll_service;
+Query OK, 1 row affected (0.02 sec)
+
+mysql> show databases;
++--------------------+
+| Database           |
++--------------------+
+| information_schema |
+| mysql              |
+| payroll_service    |
+| performance_schema |
+| sakila             |
+| sampledatabase     |
+| sampledatabase2    |
+| sampledatabase3    |
+| sys                |
+| world              |
++--------------------+
+10 rows in set (0.01 sec)
+
+mysql> use payroll_service;
+Database changed
